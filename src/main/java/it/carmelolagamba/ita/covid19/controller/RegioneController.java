fd6ced38b3bbb5a7d409508603bf92119f1055b9
carmelolg <carmelolagamba@hotmail.it>
2020-04-08 19:26:44 +0200

    - refactor

--- src/main/java/it/carmelolagamba/ita/covid19/controller/RegioneController.java
+++ src/main/java/it/carmelolagamba/ita/covid19/controller/RegioneController.java
@@ -3,7 +3,6 @@ package it.carmelolagamba.ita.covid19.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import it.carmelolagamba.ita.covid19.service.DistrictService;
 import it.carmelolagamba.ita.covid19.service.RegionService;
 import it.carmelolagamba.ita.covid19.view.AndamentoDto;
 import it.carmelolagamba.ita.covid19.view.GenericStatsDto;
@@ -11,6 +10,8 @@ import it.carmelolagamba.ita.covid19.view.GrowthRateDto;
 import it.carmelolagamba.ita.covid19.view.ResumeStatsDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -23,63 +24,63 @@ public class RegioneController {
     @ApiOperation(value = "Statistiche growth rate regionali")
     @RequestMapping(method = RequestMethod.GET, path = "/region/{name}/growthRate")
     public GrowthRateDto growthRateNazionali(@PathVariable("name") String name) {
-        return regionService.findGrowthRate(name);
+        return regionService.findGrowthRate(Optional.of(name));
     }
 
     @ApiOperation(value = "Statistiche generali regionali")
     @RequestMapping(method = RequestMethod.GET, path = "/region/{name}/stats")
     public GenericStatsDto statsRegionali(@PathVariable("name") String name) {
-        return regionService.findStats(name);
+        return regionService.findStats(Optional.of(name));
     }
 
     @ApiOperation(value = "Totale casi per regionali")
     @RequestMapping(method = RequestMethod.GET, path = "/region/{name}/total")
     public AndamentoDto statsPerProvincia(@PathVariable("name") String name) {
-        return regionService.findLast30ByDistrictName(name);
+        return regionService.findLast30TotalCases(Optional.of(name));
     }
 
     @ApiOperation(value = "Riepilogo")
     @RequestMapping(method = RequestMethod.GET, path = "/region/{name}/resume")
     public ResumeStatsDto riepilogoNazionali(@PathVariable("name") String name, @RequestParam("all") boolean all) {
-        return regionService.findResume(name, all);
+        return regionService.findResume(all, Optional.of(name));
     }
 
     @ApiOperation(value = "Totale nuovi casi regionali")
     @RequestMapping(method = RequestMethod.GET, path = "/region/{name}/total/new")
     public AndamentoDto totaleNuoviCasiNazionali(@PathVariable("name") String name) {
-        return regionService.findLast30NewPositiveByRegion(name);
+        return regionService.findLast30NewPositive(Optional.of(name));
     }
 
 
     @ApiOperation(value = "Totale ricoverati regionali")
     @RequestMapping(method = RequestMethod.GET, path = "/region/{name}/total/hospitalized")
     public AndamentoDto totaleNazionaliRicoverati(@PathVariable("name") String name) {
-        return regionService.findLast30HospitalizedByDistrictName(name);
+        return regionService.findLast30Hospitalized(Optional.of(name));
     }
 
     @ApiOperation(value = "Totale terapia intensiva regionali")
     @RequestMapping(method = RequestMethod.GET, path = "/region/{name}/total/intensive-care")
     public AndamentoDto totaleNazionaliTerapiaIntensiva(@PathVariable("name") String name) {
-        return regionService.findLast30IntensiveCureByDistrictName(name);
+        return regionService.findLast30IntensiveCure(Optional.of(name));
     }
 
     @ApiOperation(value = "Totale morti regionali")
     @RequestMapping(method = RequestMethod.GET, path = "/region/{name}/total/dead")
     public AndamentoDto totaleNazionaliMorti(@PathVariable("name") String name) {
-        return regionService.findLast30DeadByDistrictName(name);
+        return regionService.findLast30Dead(Optional.of(name));
     }
 
 
     @ApiOperation(value = "Totale tamponi regionali")
     @RequestMapping(method = RequestMethod.GET, path = "/region/{name}/total/test")
     public AndamentoDto totaleNazionaliTamponi(@PathVariable("name") String name) {
-        return regionService.findLast30TestByDistrictName(name);
+        return regionService.findLast30Test(Optional.of(name));
     }
 
     @ApiOperation(value = "Totale guariti regionali")
     @RequestMapping(method = RequestMethod.GET, path = "/region/{name}/total/recovered")
     public AndamentoDto totaleNazionaliGuariti(@PathVariable("name") String name) {
-        return regionService.findLast30RecoveredByDistrictName(name);
+        return regionService.findLast30Recovered(Optional.of(name));
     }
 
 }
