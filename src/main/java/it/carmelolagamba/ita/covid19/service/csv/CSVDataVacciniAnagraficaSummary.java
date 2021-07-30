95a0881146217fdd551ea69a68a33fcf47ec3a64
carmelolg <carmelolagamba@hotmail.it>
2021-07-30 11:05:00 +0200

    minor fix vaccine data

--- src/main/java/it/carmelolagamba/ita/covid19/service/csv/CSVDataVacciniAnagraficaSummary.java
+++ src/main/java/it/carmelolagamba/ita/covid19/service/csv/CSVDataVacciniAnagraficaSummary.java
@@ -61,28 +61,28 @@ public class CSVDataVacciniAnagraficaSummary extends AbstractCSVDataReader<DataV
 		case PREGRESSA_INFEZIONE:
 			anagraficaVacciniSummary.setPregressa_infezione(FileUtils.convertStringToInteger(value));
 			break;
-//		case CATEGORIA_OSS:
-//			anagraficaVacciniSummary
-//					.setCategoria_operatori_sanitari_sociosanitari(FileUtils.convertStringToInteger(value));
-//			break;
-//		case CATEGORIA_OVER80:
-//			anagraficaVacciniSummary.setCategoria_over80(FileUtils.convertStringToInteger(value));
-//			break;
-//		case CATEGORIA_PERSONALE_NON_SANITARIO:
-//			anagraficaVacciniSummary.setCategoria_personale_non_sanitario(FileUtils.convertStringToInteger(value));
-//			break;
-//		case CATEGORIA_RSA:
-//			anagraficaVacciniSummary.setCategoria_ospiti_rsa(FileUtils.convertStringToInteger(value));
-//			break;
-//		case CATEGORIA_FORZE_ARMATE:
-//			anagraficaVacciniSummary.setCategoria_forze_armate(FileUtils.convertStringToInteger(value));
-//			break;
-//		case CATEGORIA_PERSONALE_SCOLASTICO:
-//			anagraficaVacciniSummary.setCategoria_personale_scolastico(FileUtils.convertStringToInteger(value));
-//			break;
-//		case CATEGORIA_ALTRO:
-//			anagraficaVacciniSummary.setCategoria_altro(FileUtils.convertStringToInteger(value));
-//			break;
+		case CATEGORIA_OSS:
+			anagraficaVacciniSummary
+					.setCategoria_operatori_sanitari_sociosanitari(FileUtils.convertStringToInteger(value));
+			break;
+		case CATEGORIA_OVER80:
+			anagraficaVacciniSummary.setCategoria_over80(FileUtils.convertStringToInteger(value));
+			break;
+		case CATEGORIA_PERSONALE_NON_SANITARIO:
+			anagraficaVacciniSummary.setCategoria_personale_non_sanitario(FileUtils.convertStringToInteger(value));
+			break;
+		case CATEGORIA_RSA:
+			anagraficaVacciniSummary.setCategoria_ospiti_rsa(FileUtils.convertStringToInteger(value));
+			break;
+		case CATEGORIA_FORZE_ARMATE:
+			anagraficaVacciniSummary.setCategoria_forze_armate(FileUtils.convertStringToInteger(value));
+			break;
+		case CATEGORIA_PERSONALE_SCOLASTICO:
+			anagraficaVacciniSummary.setCategoria_personale_scolastico(FileUtils.convertStringToInteger(value));
+			break;
+		case CATEGORIA_ALTRO:
+			anagraficaVacciniSummary.setCategoria_altro(FileUtils.convertStringToInteger(value));
+			break;
 		default:
 			logger.debug("Unexpected value: {}", value);
 		}
